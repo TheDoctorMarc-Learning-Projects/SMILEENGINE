86aefe7aa1415481f27adb03c527e283be704240
marc doctor pedrosa <mdpdoctor7@hotmail.com>
2019-12-22 12:25:02 +0100

    Billboard options

--- SmileEngine/FreeBillBoard.h
+++ SmileEngine/FreeBillBoard.h
@@ -52,7 +52,9 @@ public:
 private:
 	float4x4 GetMatrix()const { return (transf.index() == 0) ? std::get<FreeTransform*>(transf)->GetGlobalMatrix() : std::get<ComponentTransform*>(transf)->GetGlobalMatrix(); };
 
-private:
+public:
 	Alignment alignment = Alignment::noAlignment;
+
+private: 
 	std::variant<FreeTransform*, ComponentTransform*> transf;
 };
