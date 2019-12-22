061d6b3c4a9e97623375b58641370083b2ef9e99
marc doctor pedrosa <mdpdoctor7@hotmail.com>
2019-12-22 17:13:26 +0100

    PlaceHolder particle emitter structure

--- SmileEngine/ComponentParticleEmitter.cpp
+++ SmileEngine/ComponentParticleEmitter.cpp
@@ -1,1 +1,62 @@
-//#include "ComponentParticleEmitter.h"
+#include "ComponentParticleEmitter.h"
+
+ComponentParticleEmitter::ComponentParticleEmitter(InitialState initialState) : initialState(initialState)
+{
+	// Only push a function if the variable changes on time
+	if (initialState.life.second.index() == 0)
+		pVariableFunctions.insert(std::pair((uint_fast8_t)0, &ComponentParticleEmitter::LifeUpdate)); 
+	if (initialState.speed.second.index() == 0)
+		pVariableFunctions.insert(std::pair((uint_fast8_t)1, &ComponentParticleEmitter::SpeedUpdate));
+/*	if (initialState.size.second.index() == 0)
+	//	pVariableFunctions.insert(std::pair((uint_fast8_t)2, &ComponentParticleEmitter::LifeUpdate));
+	if (initialState.alpha.second.index() == 0)
+	//	pVariableFunctions.insert(std::pair((uint_fast8_t)3, &ComponentParticleEmitter::LifeUpdate));
+	if (initialState.color.second.index() == 0)
+	//	pVariableFunctions.insert(std::pair((uint_fast8_t)4, &ComponentParticleEmitter::LifeUpdate));
+	if (initialState.tex.second.index() == 0)
+	//	pVariableFunctions.insert(std::pair((uint_fast8_t)5, &ComponentParticleEmitter::LifeUpdate));*/
+  
+}
+
+// -----------------------------------------------------------------
+ComponentParticleEmitter::~ComponentParticleEmitter()
+{
+	pVariableFunctions.clear(); 
+}
+
+// -----------------------------------------------------------------
+void ComponentParticleEmitter::Update(float dt)
+{
+	// Loop particles. Quickly discard inactive ones. Execute only needed functions 
+	for (int i = 0; i < particles.size() && particles.at(i).currentState.active; ++i)
+		for (auto func = pVariableFunctions.begin(); func != pVariableFunctions.end(); ++func)
+				(this->*(func->second))(particles.at(i), dt);
+			
+
+	
+}
+
+// -----------------------------------------------------------------
+void ComponentParticleEmitter::Draw()
+{
+	// TODO -> sort alive particles from far to near
+}
+
+// ----------------------------------------------------------------- Update Values
+inline void ComponentParticleEmitter::LifeUpdate(Particle& p, float dt)
+{
+	// This is placeholder 
+
+	if ((p.currentState.life -= std::get<float>(initialState.life.second)* dt) <= 0.f)
+	{
+		p.currentState.active = false; 
+	}
+}
+
+// -----------------------------------------------------------------
+inline void ComponentParticleEmitter::SpeedUpdate(Particle& p, float dt)
+{
+	// get particle free transform and add the speed to the position (locally updated!)
+ 
+}
+
--- SmileEngine/ComponentParticleEmitter.h
+++ SmileEngine/ComponentParticleEmitter.h
@@ -1,26 +1,88 @@
-/*#pragma once
+#pragma once
 
-class FreeTransform; 
-class FreeBillBoard; 
+#include <vector>
+#include <map>
+#include <variant>
+#include <any>
+
+#include "MathGeoLib/include/Math/float4.h"
+
+
+class FreeTransform;
+class FreeBillBoard;
+
+struct CurrentState
+{
+	bool active;
+	float life;
+	float3 speed;
+	float size;
+	float alpha;
+	float4 color;
+	// anim frame ? 
+	float tex;
+};
 
 struct Particle
 {
-	FreeTransform transf; 
-	FreeBillBoard billboard; 
+	/*FreeTransform transf;
+	FreeBillBoard billboard;*/
+	CurrentState currentState;
+};
+
+// Once the particle system is created, this initialState is set to the recieved values 
+// 1) Initial Value 2) Value over time, or no value (false)
+
+struct InitialState
+{
+	// Particle Variables
+	bool active = false;
+
+	// This Variables will be updated each frame if they have value over time (Current order: 0->5)
+	std::pair<float, std::variant<float, bool>> life = std::pair(100.f, 1.f);
+	std::pair<float3, std::variant<float3, bool>> speed = std::pair(float3::one, float3::one);
+	std::pair<float, std::variant<float, bool>> size = std::pair(1.f, 0.f);
+	std::pair<float, std::variant<float, bool>> alpha = std::pair(1.f, 0.f);
+	std::pair<float4, std::variant<float4, bool>> color = std::pair(float4::zero, float4::zero);
+	std::pair<float, std::variant<float, bool>> tex = std::pair(0.f, 0.f);
+
+};
+
+struct emissionData
+{
+	uint_fast8_t maxParticles = 100;
+	bool loop = true;
+	float rate = 1.f; 
 };
 
 enum emmissionShape { CIRCLE, CUBE, CONE };
 struct emmssionMode { emmissionShape shape; float angle, radius; };
 
+class ComponentParticleEmitter; 
+typedef void (ComponentParticleEmitter::*function)(Particle& p, float dt);
+
 class ComponentParticleEmitter
 {
 public: 
+	ComponentParticleEmitter(InitialState initialState);
+	~ComponentParticleEmitter();
+public: 
 	enum blendMode{ ADDITIVE, ALPHA_BLEND };
-	enum lightMode { PER_EMITTER, PER_PARTICLE, NONE};
-	enum particleMode { COLOR, TEXTURE, ANIM_TEXTURE};
+	enum lightMode { PER_EMITTER, PER_PARTICLE, NONE}; // TODO: how to handle this?
 	emmssionMode emmissionMode;
+	emissionData emissionData; 
+
+public: 
+	void Update(float dt); 
 
 private: 
-	Particle particles[150]; 
+	void Draw(); 
+	inline void SpeedUpdate(Particle& p, float dt);
+	inline void LifeUpdate(Particle& p, float dt);
 
-};*/
+private: 
+	std::vector<Particle> particles; 
+	InitialState initialState;
+	std::map<uint_fast8_t, function> pVariableFunctions; // They co-relate by order to particle state variables (Current order: 0->5)
+
+};
